8f27f2cb284ce9276542393e6579dc864bb2f232
Mahir Yapar <mahiryapar2453@gmail.com>
2024-11-30 11:20:03 +0300

    Bakiye yükle sayfası açıldı. Vendor dosyası yüklendi.

--- pages/bakiye_yukle.php
+++ pages/bakiye_yukle.php
@@ -1,36 +1,12 @@
 <?php 
 session_start();
-
-
-if(isset($_SESSION['nickname'])){
-    $nickname = $_SESSION['nickname'];
-    $role = $_SESSION['role'];
-    include '../backend/fetch_image.php';
-}
-else{
-    $_SESSION['role'] = "guest";
-}
-$message = null;
-if(isset($_SESSION['flash_message'])){
-    $message = $_SESSION['flash_message'];
-    unset($_SESSION['flash_message']);
-}
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Yemekhane</title>
-    <style>
-        #cikis{
-            display:none;
-            margin-left:20vw;
-            margin-right:20vw;
-            margin-top:3vh;
-        }
-    </style>
+    <title>Bakiye Yükle</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
@@ -43,10 +19,8 @@ if(isset($_SESSION['flash_message'])){
                 <li class="liler" id = "ogrenci-li"><a class="linkler" href="yemek_takvimi.php">Yemek Takvimi</a></li>
                 <li class="liler" id = "admin-li"><a class="linkler" href="yemek_takvimi.php">Yemek Takvimi</a></li>
                 <li class="liler" id = "ogrenci-li"><a class="linkler" href="#">Öğün Satın Al</a></li>
-                <li class="liler" id="signin"><a class="linkler" href="giris_kayit.php?giris=1">Giriş Yap</a></li>
                 <li class="prfl" ><div id="prfl-foto"><img src="<?php echo $_SESSION['pp']; ?>"alt="Profil Fotoğrafı" id="prfl-foto-img"></div></li>
-                <li class="liler" id="signin"><a class="linkler" href="giris_kayit.php?giris=0">Kaydol</a></li>
-                <li class="liler" id = "ogrenci-li"><a class="linkler" href="bakiye_yukle.php">Bakiye Yükle</a></li>
+                <li class="liler" id = "ogrenci-li"><a class="linkler" href="#">Bakiye Yükle</a></li>
                 <li class="liler" id="admin-li"><a class="linkler" href="#">Yemekhanem</a></li>
                 <li class="liler" id="ogrenci-li"><a class="linkler" href="#">Yemekhanem</a></li>
                 <li class="liler" id="admin-li"><a class="linkler" href="#">Öğrenciler</a></li>
@@ -55,26 +29,15 @@ if(isset($_SESSION['flash_message'])){
                 <li class="liler" id="admin-li"><a class="linkler" href="../backend/logout.php">Çıkış</a></li>
         </nav>
         <div id="icerik">
-            <div id="cikis" class="alert alert-success">
-                <strong>Logout!</strong> Başarıyla Çıkış Yapıldı.
-            </div>
+
+
         </div>
     </div>
     <script>
-        window.onload = () => {
-            if(<?php echo json_encode($message);?> != null){
-                document.getElementById('cikis').style.display = 'block';
-                setTimeout(() => {
-                    document.getElementById('cikis').style.display = 'none';
-                }, 2000);
-        }
-        };
     </script>
     <script src="../js/app.js"></script>
     <link rel="stylesheet" href="../css/design.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
-
-
 
